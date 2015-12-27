09386145315feb836e66131cfb6672e1505497d3
Alberto Vecina Sánchez <albertovecina@gmail.com>
2015-12-27 11:56:27 +0100

    *Updated Twitter API Keys *Optimized Twitter search

--- app/src/main/java/com/vsa/tweetclock/domain/interactor/TweetClockInteractorImpl.java
+++ app/src/main/java/com/vsa/tweetclock/domain/interactor/TweetClockInteractorImpl.java
@@ -19,47 +19,71 @@ import rx.Observable;
  */
 public class TweetClockInteractorImpl implements TweetClockInteractor {
 
-    private static final String TIME_PATTERN_24_H = "HH:mm";
-    private static final String TIME_PATTERN_12_H = "hh:mm";
-    private static final String TIME_PATTERN_12_H_A = "hh:mm a";
+    private static final String QUERY_TEXT_PATTERN_IT_IS = "\"It is %s and\"";
+    private static final String QUERY_TEXT_PATTERN_ITS = "\"It's %s and\"";
+    private static final String QUERY_TEXT_PATTERN_NON_EXACT = "It's \"%s\" and";
+    private static final String TIME_PATTERN_24_H = "H:mm";
+    private static final String TIME_PATTERN_24_HH = "HH:mm";
+    private static final String TIME_PATTERN_24_H_A = "H:mm a";
+    private static final String TIME_PATTERN_24_HH_A = "HH:mm a";
+    private static final String TIME_PATTERN_12_H = "h:mm";
+    private static final String TIME_PATTERN_12_HH = "hh:mm";
+    private static final String TIME_PATTERN_12_H_A = "h:mm a";
+    private static final String TIME_PATTERN_12_HH_A = "hh:mm a";
+    private static final String[] TIME_PATTERNS = new String[]{
+            TIME_PATTERN_24_H, TIME_PATTERN_24_HH,
+            TIME_PATTERN_24_H_A, TIME_PATTERN_24_HH_A,
+            TIME_PATTERN_12_H, TIME_PATTERN_12_HH,
+            TIME_PATTERN_12_H_A, TIME_PATTERN_12_HH_A};
 
-    private SimpleDateFormat mDateFormat = new SimpleDateFormat(TIME_PATTERN_24_H, Locale.US);
+    private SimpleDateFormat mDateFormat = new SimpleDateFormat(TIME_PATTERN_24_HH, Locale.US);
     private Repository mRepository;
 
     public TweetClockInteractorImpl(Repository repository) {
         mRepository = repository;
     }
 
+    /**
+     * Request the guest session using the Twitter API
+     *
+     * @return An Observable that emits a {@link AppSession} object
+     */
     @Override
     public Observable<AppSession> loginGuest() {
         return mRepository.doGuestLogin();
     }
 
+    /**
+     * Search a tweet with that matches the predefined message formats
+     *
+     * @param session {@link AppSession} Object that represets the Twitter guest session
+     * @return An Observable that emits a {@link TweetTic}
+     */
     @Override
     public Observable<TweetTic> searchTimeTweet(AppSession session) {
         Date currentDate = mRepository.getCurrentDate();
-        mDateFormat.applyPattern(TIME_PATTERN_24_H);
-        String time24HFormat = mDateFormat.format(currentDate);
-        mDateFormat.applyPattern(TIME_PATTERN_12_H);
-        String time12HFormat = mDateFormat.format(currentDate);
-        mDateFormat.applyPattern(TIME_PATTERN_12_H_A);
-        String time12HFormatA = mDateFormat.format(currentDate);
-        final String pattern1 = "\"It is " + time24HFormat + " and\"";
-        final String pattern2 = "\"It is " + time12HFormat + " and\"";
-        final String pattern3 = "\"It is " + time12HFormatA + " and\"";
-        final String pattern4 = "\"It's " + time24HFormat + " and\"";
-        final String pattern5 = "\"It's " + time12HFormat + " and\"";
-        final String pattern6 = "\"It's " + time12HFormatA + " and\"";
-        final String pattern7 = "It's \"" + time24HFormat + "\" and";
+        String queryItIs = buildQuery(currentDate,
+                new String[]{QUERY_TEXT_PATTERN_IT_IS},
+                TIME_PATTERNS);
+        String queryIts = buildQuery(currentDate,
+                new String[]{QUERY_TEXT_PATTERN_ITS},
+                TIME_PATTERNS);
+        String queryNonExact = buildQuery(currentDate,
+                new String[]{QUERY_TEXT_PATTERN_NON_EXACT},
+                new String[]{TIME_PATTERN_12_H}); //QUERY FOR NON-EXACT PHRASE
         return Observable.combineLatest(
-                mRepository.searchTweet(session, pattern1),
-                mRepository.searchTweet(session, pattern2),
-                mRepository.searchTweet(session, pattern3),
-                mRepository.searchTweet(session, pattern4),
-                mRepository.searchTweet(session, pattern5),
-                mRepository.searchTweet(session, pattern6),
-                mRepository.searchTweet(session, pattern7),
-                (this::manageResults));
+                Observable.zip( // LAUNCH TWO REQUESTS TO AVOID THE QUERY LENGTH LIMITATION
+                        mRepository.searchTweet(session, queryItIs),
+                        mRepository.searchTweet(session, queryIts),
+                        (resultsA, resultsB) -> new ArrayList() {
+                            {
+                                addAll(resultsA);
+                                addAll(resultsB);
+                            }
+                        }
+                ),
+                mRepository.searchTweet(session, queryNonExact), //LAUNCH NON-STRICT QUERY TO ENSURE ONE RESULT AT LEAST
+                (this::pickTweet));
     }
 
     @Override
@@ -68,23 +92,37 @@ public class TweetClockInteractorImpl implements TweetClockInteractor {
         return 60 - seconds;
     }
 
-    private TweetTic manageResults(List<TweetTic> pattern1, List<TweetTic> pattern2,
-                                   List<TweetTic> pattern3, List<TweetTic> pattern4,
-                                   List<TweetTic> pattern5, List<TweetTic> pattern6,
-                                   List<TweetTic> pattern7) {
-        List<TweetTic> results = new ArrayList<>();
-        results.addAll(pattern1);
-        results.addAll(pattern2);
-        results.addAll(pattern3);
-        results.addAll(pattern4);
-        results.addAll(pattern5);
-        results.addAll(pattern6);
-        if (results.isEmpty())
-            results = pattern7;
+    private String buildQuery(Date currentDate, String[] messageFormats, String[] timePatterns) {
+        final String or = " OR ";
+        StringBuilder stringBuilderQuery = new StringBuilder();
+        for (String messageFormat : messageFormats) {
+            for (String timePattern : timePatterns) {
+                mDateFormat.applyPattern(timePattern);
+                String query = String.format(messageFormat, mDateFormat.format(currentDate));
+                stringBuilderQuery.append(query);
+                stringBuilderQuery.append(or);
+            }
+        }
+        return stringBuilderQuery.substring(0, stringBuilderQuery.length() - or.length());
+    }
+
+    /**
+     * Picks the best choice from the results. Most accuracy first, most retwitted first, recent first
+     *
+     * @param bestCaseResults       {@link TweetTic} List that contains the results of the distinct accepted text formats
+     * @param nonStrictQueryResults {@link TweetTic} List that contains the results for an alternative query with a non-strict format
+     * @return
+     */
+    private TweetTic pickTweet(List<TweetTic> bestCaseResults, List<TweetTic> nonStrictQueryResults) {
+        List<TweetTic> results;
+        if (!bestCaseResults.isEmpty())
+            results = bestCaseResults;
+        else
+            results = nonStrictQueryResults;
         Collections.sort(results, (lhs, rhs) -> {
             int ret = rhs.getRetweetCount() - lhs.getRetweetCount();
             if (ret == 0)
-                return lhs.getCreationJavaDate().compareTo(rhs.getCreationJavaDate());
+                return rhs.getCreationJavaDate().compareTo(lhs.getCreationJavaDate());
             return ret;
         });
         if (results.size() > 0)
